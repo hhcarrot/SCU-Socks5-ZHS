624bdf324bfce43cd130843ee1ff45839dfa31cc
zora <617420858@qq.com>
2016-12-15 19:57:35 +0800

    GUI
    logical part is already. interface has not completed yet

--- GUI.py
+++ GUI.py
@@ -2,106 +2,155 @@
 from tkinter import *
 import tkinter
 
+
+pubList = []
+priList = []
+#for test
+for i in range(20):
+	pubList.append('(pubic)'+str(i*100)+'______________________________________!!!')
+	priList.append('(private)'+str(i*100)+'______________________________________!!!')
+
+whichList = ('Public servers', pubList)
+
+
 def hello():
 	print('hello menu')
 	
-def isEncode():
-	#v.set('check Tkinter')	
-	print(varEncode.get())
+	
+def freshList(v):
+	global whichList
+	if (v == "Public Servers"):
+		whichList = ('Public Servers', pubList)
+		enUsername['state'] = ACTIVE
+		enPassword['state'] = ACTIVE
+		butEncode['state'] = ACTIVE
+	else:
+		whichList = ('Private Servers', priList)
+		enUsername['state'] = DISABLED
+		enPassword['state'] = DISABLED
+		butEncode['state'] = DISABLED
+	print(v+'loaded..')
+	init()
 	
 def submit():
-   print(v1.get())
-   print(v2.get())
+	print('select:')
+	print(whichList[0])
+	print(lb.curselection())
+	print('username: ')
+	print(varUsername.get())
+	print('password: ')
+	print(varPassword.get())
+	print('encode: ')
+	print(varEncode.get())
+	print('port: ')
+	print(spinPort.get())
+	print('note: ')
+	print(varNote.get())
 	
+	
+#   print(v2.get())
+
 root = Tk()
+root.title("SHZHsocks")
 varChooseList = StringVar(root)
-varChooseList.set('公用服务器')
-om = OptionMenu(root, varChooseList, '公用服务器', '私有服务器')
-om.grid(row = 2)
+om = OptionMenu(root, varChooseList, 'Public Servers', 'Private Servers', command = freshList)
 
-
-#列表
 lb = Listbox(root, height = 10)
-for i in range(20):
-	lb.insert(END, str(i*100)+'______________________________________!!!')
-print(lb.curselection())
-#print(lb.get(lb.curselection()))
-
-
 sl1 = Scrollbar(root)
-lb.configure(yscrollcommand = sl1.set)
-sl1['command'] = lb.yview
-sl1.grid(row = 5, column = 9, sticky = 'nsew')
-
-
-
 sl2 = Scrollbar(root, orient = 'horizontal')
-lb['xscrollcommand'] = sl2.set
-sl2['command'] = lb.xview
-sl2.grid(row = 6, column = 0, columnspan = 8, sticky = 'new')
-
-
-lb.grid(row = 5, columnspan = 10)
-
-#menubar = Menu(root)
-#filemenu = Menu(menubar, tearoff = 0)
-#for item in ['a', 'b', 'c']:
-#	filemenu.add_command(label = item, command = hello)
-#	filemenu.add_separator()
-#menubar.add_cascade(label = 'Language', menu = filemenu)
-#root['menu'] = menubar
-
 
 fm = []
 fm.append(Frame(height = 10, width = 50, bg = 'gray'))
 fm.append(Frame(height = 10, width = 50))
-Label(fm[0],text = '私有服务器配置',bg = 'gray').grid(row = 0, column = 0,columnspan = 2)
 
-labUsername =Label(fm[0], text="用户名:", bg = 'gray')
-labUsername.grid(row=2, column=0, padx=5, pady=5, sticky=W)
-#绑定对象到Entry
+labUsername =Label(fm[0], text="Username:", bg = 'gray')
 varUsername = StringVar()	
 enUsername = Entry(fm[0], textvariable=varUsername)
-enUsername.grid(row=2, column=1, sticky='ew', columnspan=2)
-	
-labPassword = Label(fm[0], text="密码:",bg = 'gray')
-labPassword.grid(row=3, column=0, padx=5, pady=5, sticky=W)
+
+labPassword = Label(fm[0], text="Password:",bg = 'gray')
 varPassword = StringVar()
 enPassword = Entry(fm[0], textvariable=varPassword)
 enPassword['show'] = '*'
-enPassword.grid(row=3, column=1, sticky='ew', columnspan=2)
 
 varEncode = StringVar()
-varEncode.set('check Tkinter')
-Checkbutton(fm[0], text = '加密',bg = 'gray', variable = varEncode, command = isEncode).grid(row=4)
+varEncode.set(0)
+butEncode = Checkbutton(fm[0], text = 'Encode',bg = 'gray', variable = varEncode)
 
-
-
-
-fm[0].grid(row = 2, column = 10, rowspan = 6)
-
-labPort =Label(fm[1], text="代理端口:")
-labPort.grid(row=0, column=0, padx=4, pady=5, sticky=W)
-
+labPort =Label(fm[1], text="Port:")
 varPort = StringVar() 
 spinPort = Spinbox(fm[1], from_ = 0, to = 65535, increment = 1)
-spinPort.grid(row=0, column=1, columnspan =2)
+labNote =Label(fm[1], text="Note:")
 
-labNote =Label(fm[1], text="备注:")
-labNote.grid(row=1, column=0, padx=5, pady=5, sticky=W)
-#绑定对象到Entry
 varNote = StringVar()	
 enNote = Entry(fm[1], textvariable=varNote)
-enNote.grid(row=1, column=1, sticky='ew', columnspan=3)
+butSubmit = Button(fm[1], text="Submit", command=submit, default='active')
+butQuit = Button(fm[1], text="Quit", command=quit)
 
-fm[1].grid(row = 6, column = 10, rowspan=5)
 
-butSubmit = Button(fm[1], text="应用", command=submit, default='active')
-butSubmit.grid(row=3, column=1)
-butQuit = Button(fm[1], text="退出", command=quit)
-butQuit.grid(row=3, column=2, columnspan = 2)
+
+	
+def init():
+	global varChooseList
+	global lb
+	varChooseList.set(whichList[0])
+	print(whichList[0])
+	om.grid(row = 2)
+#列表
+	
+	lb.delete(0, lb.size())
+	print(whichList[1])
+	for item in whichList[1]:
+		lb.insert(END, item)
+	print(lb.curselection())
+#print(lb.get(lb.curselection()))
+
+
+	lb.configure(yscrollcommand = sl1.set)
+	sl1['command'] = lb.yview
+	sl1.grid(row = 5, column = 9, sticky = 'nsew')
+
+
+
+	lb['xscrollcommand'] = sl2.set
+	sl2['command'] = lb.xview
+	sl2.grid(row = 6, column = 0, columnspan = 8, sticky = 'new')
+
+
+	lb.grid(row = 5, columnspan = 10)
+
+
+	Label(fm[0],text = 'Private Servers Configuration',bg = 'gray').grid(row = 0, column = 0,columnspan = 2)
+
+	labUsername.grid(row=2, column=0, padx=5, pady=5, sticky=W)
+	#绑定对象到Entry
+	
+	enUsername.grid(row=2, column=1, sticky='ew', columnspan=2)
+	
+	labPassword.grid(row=3, column=0, padx=5, pady=5, sticky=W)
+	enPassword.grid(row=3, column=1, sticky='ew', columnspan=2)
+
+	
+	butEncode.grid(row=4)
+
+	fm[0].grid(row = 2, column = 10, rowspan = 6)
+
+	labPort.grid(row=0, column=0, padx=4, pady=5, sticky=W)
+
+	spinPort.grid(row=0, column=1, columnspan =2)
+
+	labNote.grid(row=1, column=0, padx=5, pady=5, sticky=W)
+#绑定对象到Entry
+
+	enNote.grid(row=1, column=1, sticky='ew', columnspan=3)
+
+	fm[1].grid(row = 6, column = 10, rowspan=5)
+
+	butSubmit.grid(row=3, column=1)
+	butQuit.grid(row=3, column=2, columnspan = 2)
 
 #lb1.grid()
 #lb2.grid(row = 0, column = 1)
-root.columnconfigure(0, minsize = 100)
-root.mainloop()
+	root.columnconfigure(0, minsize = 100)
+	root.mainloop()
+	
+init()
